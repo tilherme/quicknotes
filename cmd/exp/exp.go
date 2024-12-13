b49fff524e4b0aec3cb77c63d015b32f4c9a1291
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-12-12 23:04:51 -0300

    feat: specifying errors

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,22 +1,48 @@
 package main
 
 import (
-	"log/slog"
+	"errors"
+	"fmt"
 	"os"
 )
 
+type CustomError struct {
+	msg  string
+	code int
+}
+
+var bad = &CustomError{msg: "vai dar merda", code: 400}
+
+func (c *CustomError) Error() string {
+	return fmt.Sprintf("%s, %d", c.msg, c.code)
+
+}
+func NewCustomeError(msg string, code int) error {
+	return &CustomError{msg: msg, code: code}
+}
+func process() (string, error) {
+	f, err := os.Open("../../config.son")
+	if err != nil {
+		// return "", errors.New("erro new")
+		// return "", fmt.Errorf("vai dar não")
+		// return "", NewCustomeError("vai dar merda", 400)
+		// return "", bad
+		return "", fmt.Errorf("vai dar não; (%w)", err)
+
+	}
+	return f.Name(), nil
+}
 func main() {
-	// log.Println("Log padrao")
-	// h := slog.NewTextHandler(os.Stdout, nil)
-	h := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level:     slog.LevelDebug,
-		AddSource: true,
-	})
-
-	log := slog.New(h).With("app", "exp")
-	log.Info("info message", "id", 1)
-	log.Debug("debug message")
-	log.Warn("warn message")
-	log.Error("warn message")
-
+	r, err := process()
+	if err != nil {
+		// var bad *CustomError
+		// if errors.As(err, &bad) {
+		// 	fmt.Println(bad.code)
+		// 	return
+		// }
+		// fmt.Println(err)
+		fmt.Println(errors.Unwrap(err))
+		return
+	}
+	fmt.Println(r, "fff")
 }
