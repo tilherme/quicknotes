43ff2fbd58f3768d695921dfd591a41f056557be
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-12-18 18:54:59 -0300

    feat: connect bdb and packege pgx

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,48 +1,18 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
-	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
-type CustomError struct {
-	msg  string
-	code int
-}
-
-var bad = &CustomError{msg: "vai dar merda", code: 400}
-
-func (c *CustomError) Error() string {
-	return fmt.Sprintf("%s, %d", c.msg, c.code)
-
-}
-func NewCustomeError(msg string, code int) error {
-	return &CustomError{msg: msg, code: code}
-}
-func process() (string, error) {
-	f, err := os.Open("../../config.son")
-	if err != nil {
-		// return "", errors.New("erro new")
-		// return "", fmt.Errorf("vai dar não")
-		// return "", NewCustomeError("vai dar merda", 400)
-		// return "", bad
-		return "", fmt.Errorf("vai dar não; (%w)", err)
-
-	}
-	return f.Name(), nil
-}
 func main() {
-	r, err := process()
+	dbUrl := "postgres://postgres:secret@localhost:5432/postgres"
+	con, err := pgx.Connect(context.Background(), dbUrl)
 	if err != nil {
-		// var bad *CustomError
-		// if errors.As(err, &bad) {
-		// 	fmt.Println(bad.code)
-		// 	return
-		// }
-		// fmt.Println(err)
-		fmt.Println(errors.Unwrap(err))
-		return
+		panic(err)
 	}
-	fmt.Println(r, "fff")
+	fmt.Println("Conecxão feita com sucesso!!! ")
+	defer con.Close(context.Background())
 }
