3e9dba996e638634730bc3d3a0b19c580653dd2d
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-12-26 17:12:20 -0300

    feat: create table posts in go

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,16 +3,42 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	var err error
+	var conn *pgx.Conn
+
 	dbUrl := "postgres://postgres:secret@localhost:5432/postgres"
-	con, err := pgx.Connect(context.Background(), dbUrl)
+	conn, err = pgx.Connect(context.Background(), dbUrl)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Conecxão feita com sucesso!!! ")
-	defer con.Close(context.Background())
+
+	defer conn.Close(context.Background())
+	fmt.Println("Conecxão feita com sucesso!!! ", conn)
+
+	createTable(conn)
+}
+
+func createTable(conn *pgx.Conn) {
+	query := `
+		CREATE TABLE posts (
+		id SERIAL PRIMARY KEY,
+		title TEXT NOT NULL,
+		content TEXT,
+		author TEXT NOT NULL
+		);
+	`
+
+	_, err := conn.Exec(context.Background(), query)
+	if err != nil {
+		log.Fatalf("Error creating table: %v\n", err)
+		return
+	}
+
+	fmt.Println("Table 'posts' created")
 }
